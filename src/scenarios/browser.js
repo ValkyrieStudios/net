304578af875036f388c9a1b6864861f7ff82f17c
Peter Vermeulen <contact@valkyriestudios.be>
2017-10-21 03:21:04 +0200

    fix typo

--- src/scenarios/browser.js
+++ src/scenarios/browser.js
@@ -1,7 +1,7 @@
 import { isObject } from '@valkyriestudios/utils/object';
 import { isArray } from '@valkyriestudios/utils/array';
 import Scenario from '../blueprints/Scenario';
-import Response from '../blueprints/Scenario';
+import Response from '../blueprints/Response';
 import { METHODS_ALLOWED_BODY } from '../constants';
 
 function getResponseHeaders (req) {
