c22a142839345c7e987372f797010623a0eb0343
Peter Vermeulen <contact@valkyriestudios.be>
2023-04-01 11:14:54 +0200

    [ops] Cleanup serializeUrl helper

--- src/serialize/serializeURL.js
+++ src/serialize/serializeURL.js
@@ -3,40 +3,30 @@
 import Is   from '@valkyriestudios/utils/is';
 import join from '@valkyriestudios/utils/array/join';
 
-//  Serialize a key-value pair object into a query string
-//  e.g : {a:1,b:2} => 'a=1&b=2'
-function serializeQueryParameters (params) {
-    if (Is.Object(params)) {
-        return Object.keys(params).reduce((acc, key) => {
-            acc.push(`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-            return acc;
-        }, []).join('&');
-    } else if (Is.String(params)) {
-        return params.trim();
-    } else {
-        throw new TypeError('Net:serializeURL expects params option to be either a string or an object');
-    }
-}
-
 export default function serializeURL (url, options, NET_CONFIG) {
     //  Check for possibly configured base domain (e.g: https://www.google.com)
-    const base = Object.prototype.hasOwnProperty.call(options, 'base')
-        ? options.base
-        : NET_CONFIG.base;
+    const base = Is.Object(options) && options.hasOwnProperty('base') ? options.base : NET_CONFIG.base;
 
     //  Build base url
     let serialized_url = join([base, url], {delim: ''});
 
     //  Check for possibly configured querystring parameters
-    const params = Object.prototype.hasOwnProperty.call(options, 'params')
-        ? options.params
-        : NET_CONFIG.params;
-    if (params) {
-        const serialized_params = serializeQueryParameters(params);
-        if (Is.NotEmptyString(serialized_params)) {
-            serialized_url = join([serialized_url, serialized_params], {delim: '?'});
-        }
+    let params = Is.Object(options) && options.hasOwnProperty('params') ? options.params : NET_CONFIG.params;
+    if (Is.String(params)) {
+        params = params.trim();
+    } else if (Is.NotEmptyObject(params)) {
+        params = Object.keys(params).reduce((acc, key) => {
+            if (!Is.NotEmptyString(key)) return acc;
+
+            acc.push(`${encodeURIComponent(key).trim()}=${encodeURIComponent(params[key]).trim()}`);
+            return acc;
+        }, []).join('&');
+    } else {
+        params = false;
     }
+
+    //  If params is filled in after serialization
+    if (params) serialized_url = `${serialized_url}?${params}`;
 
     return serialized_url;
 }
