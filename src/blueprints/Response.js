7c9132cf2713dedcc1c943c8ad2587970477c2f7
Peter Vermeulen <contact@valkyriestudios.be>
2023-04-01 11:23:46 +0200

    [ops] Small response cleanup, add sanity check for options

--- src/blueprints/Response.js
+++ src/blueprints/Response.js
@@ -1,20 +1,21 @@
 'use strict';
 
+import Is               from '@valkyriestudios/utils/is';
 import {RESPONSE_TYPES} from '../constants';
 
 //  Parses the response to a request and returns
-//  an object of form {status, data, headers, options}
+//  an object of form {status, data, headers}
 export default class Response {
 
     constructor (code, msg = '', data = undefined, headers = {}, options = {}) {
         this.status     = code && msg ? {code, msg} : undefined;
-        this.headers    = headers;
+        this.headers    = Is.Object(headers) ? headers : {};
 
         this.data = {};
         if (data) {
             try {
                 if (
-                    options.responseType === RESPONSE_TYPES.JSON ||
+                    (Is.Object(options) && options.responseType === RESPONSE_TYPES.JSON) ||
                     ((headers['content-type'] || '').indexOf('application/json') > -1)
                 ) {
                     this.data = JSON.parse(data);
